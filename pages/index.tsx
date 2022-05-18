761246f75dc457c522e0df5cc38481a84f66a416
Ivan Tkachuk <ivtkac@gmail.com>
2022-05-18 15:09:26 +0300

    add form to comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import CommentForm from 'components/CommentForm'
 import Layout from 'components/Layout'
 
 const Home = () => {
   return (
     <Layout>
       <p>Its home page</p>
+      <CommentForm />
     </Layout>
   )
 }
