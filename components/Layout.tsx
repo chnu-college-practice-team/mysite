2f33b8d5027f901bb918ddc814d8a21554d8c377
jadatix <andrijlasenko@gmail.com>
2022-05-21 12:59:29 +0300

    add command palette

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import CommandPalette from './CommandPalette'
 import Header from './Headers'
+import useSWR from 'swr'
+import type { User } from '@prisma/client'
+import fetcher from 'lib/fetcher'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { data, error } = useSWR < { users: User[] }>(
+    '/api/user',
+    fetcher,
+  )
+  if (error) return <>An error has occurred.</>
+  if (!data) return <>Loading...</>
+
   return (
     <>
       <Header />
+      <CommandPalette users={data.users}/>
       <main className="container">{children}</main>
     </>
   )
